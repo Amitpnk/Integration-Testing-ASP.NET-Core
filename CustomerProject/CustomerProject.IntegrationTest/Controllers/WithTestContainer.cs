78d3de5de3751c0027508ba4ac4d0b19f2d22469
Amit P Naik <amit.naik8103@gmail.com>
2024-06-20 23:42:22 +0530

    test: docker container test

---
+++ CustomerProject/CustomerProject.IntegrationTest/Controllers/WithTestContainer.cs
@@ -0,0 +1,62 @@
+﻿using CustomerProject.IntegrationTest.Fixtures;
+using CustomerProject.IntegrationTest.Helper;
+using CustomerProject.WebApi.Models;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerProject.IntegrationTest.Controllers
+{
+    public class WithTestContainer : IClassFixture<DockerWebApplicationFactoryFixture>
+    {
+        private readonly DockerWebApplicationFactoryFixture _factory;
+        private readonly HttpClient _client;
+
+        public WithTestContainer(DockerWebApplicationFactoryFixture factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task OnGetStudent_WhenExecuteController_ShouldreturnTheExpecedStudet()
+        {
+            // Arrange
+
+            // Act
+            var response = await _client.GetAsync(HttpHelper.Urls.GetAllCustomers);
+            var result = await response.Content.ReadFromJsonAsync<List<Customers>>();
+
+            // Assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+            result?.Count.Should().Be(_factory.InitialStudentsCount);
+            result.Should()
+                .BeEquivalentTo(DataFixture.GetCustomers(_factory.InitialStudentsCount), options => options.Excluding(t => t.CustomerId));
+        }
+
+        [Fact]
+        public async Task OnAddStudent_WhenExecuteController_ShouldStoreInDb()
+        {
+            // Arrange
+            var newStudent = DataFixture.GetCustomer(true);
+
+            // Act
+            var request = await _client.PostAsync(HttpHelper.Urls.AddCustomer, HttpHelper.GetJsonHttpContent(newStudent));
+            var response = await _client.GetAsync($"{HttpHelper.Urls.GetCustomer}/{_factory.InitialStudentsCount + 1}");
+            var result = await response.Content.ReadFromJsonAsync<Customers>();
+
+            // Assert
+            request.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+
+            result?.CustomerCode.Should().Be(newStudent.CustomerCode);
+            result?.CustomerName.Should().Be(newStudent.CustomerName);
+        }
+    }
+}
---
+++ CustomerProject/CustomerProject.IntegrationTest/Fixtures/DockerWebApplicationFactoryFixture.cs
@@ -0,0 +1,63 @@
+﻿using CustomerProject.WebApi.DbContexts;
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace CustomerProject.IntegrationTest.Fixtures
+{
+    public class DockerWebApplicationFactoryFixture : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private MsSqlContainer _dbContainer;
+        public int InitialStudentsCount { get; } = 3;
+
+        public DockerWebApplicationFactoryFixture()
+        {
+            _dbContainer = new MsSqlBuilder().Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            var connectionString = _dbContainer.GetConnectionString();
+            base.ConfigureWebHost(builder);
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveAll(typeof(DbContextOptions<CustomerDbContext>));
+                services.AddDbContext<CustomerDbContext>(options =>
+                {
+                    options.UseSqlServer(connectionString);
+                });
+            });
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+
+            using (var scope = Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var cntx = scopedServices.GetRequiredService<CustomerDbContext>();
+
+                await cntx.Database.EnsureCreatedAsync();
+
+                await cntx.Customers.AddRangeAsync(DataFixture.GetCustomers(InitialStudentsCount));
+                await cntx.SaveChangesAsync();
+            }
+        }
+
+        public async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _dbContainer.StopAsync();
+        }
+    }
+}
