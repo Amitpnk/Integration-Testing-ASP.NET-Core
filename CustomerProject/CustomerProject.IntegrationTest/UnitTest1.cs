077710e76adbd135afa3fe4b19e53a9488646c2e
Amit P Naik <amit.naik8103@gmail.com>
2024-06-20 22:05:22 +0530

    test: adding test case for example

--- CustomerProject/CustomerProject.IntegrationTest/UnitTest1.cs
+++ CustomerProject/CustomerProject.IntegrationTest/UnitTest1.cs
@@ -1,11 +1,21 @@
-namespace CustomerProject.IntegrationTest
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace CustomerProject.IntegrationTest;
 
-        }
+public class UnitTest1
+{
+    public UnitTest1()
+    {
+
+    }
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
     }
 }
