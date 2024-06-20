20292063d626f7349f8f9abdab6753d10ca888c1
Amit P Naik <amit.naik8103@gmail.com>
2024-06-20 23:31:28 +0530

    feat: fix issue

--- CustomerProject/Customer.WebApi/Models/Customers.cs
+++ CustomerProject/Customer.WebApi/Models/Customers.cs
@@ -1,7 +1,10 @@
-﻿namespace CustomerProject.WebApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerProject.WebApi.Models;
 
 public class Customers
 {
+    [Key]
     public int CustomerId { get; set; }
     public string CustomerCode { get; set; }
     public string CustomerName { get; set; }
--- CustomerProject/Customer.WebApi/Program.cs
+++ CustomerProject/Customer.WebApi/Program.cs
@@ -2,26 +2,33 @@ using CustomerProject.WebApi.DbContexts;
 using CustomerProject.WebApi.Services;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<CustomerDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddTransient<ICustomerService, CustomerService>();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace CustomerProject;
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<CustomerDbContext>(options =>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddTransient<ICustomerService, CustomerService>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
